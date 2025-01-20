d7d94eb0f4a04ae50df939ea8cda04458be86108
harmony <128433295+kawtinx@users.noreply.github.com>
2025-01-20 20:31:31 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 
 // Owner
 global.owner = [
-['212717457920', 'Noureddine', true]
+['212621469696', 'HARMONY DEVE', true]
 ]
 global.mods = []
 global.prems = []
